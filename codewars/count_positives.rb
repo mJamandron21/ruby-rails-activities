46097fee78cfe5391adad04657e549bb3d6bd36a
mJamandron21 <mjamandron@gmail.com>
2022-06-02 15:54:31 +0800

    Adding count positives sum negatives codewars.

---
+++ codewars/count_positives.rb
@@ -0,0 +1,8 @@
+def  count_positives_sum_negatives(lst)
+    sum = 0
+    positive = lst.count {|i| i > 0}
+    negative = lst.each {|i| sum += i if i < 0 }
+    [positive,negative]
+
+    # lst.empty? ? [] :[lst.count{|n| n > 0},lst.each{|n| n < 0}.sum]
+end
